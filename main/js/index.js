9d6d7209ddb0b2c05e5efe8cbc4625eca01ced08
Andreii Golovan <andreii.golovan@gmail.com>
2018-07-03 02:15:16 +0300

    sections add

---
+++ main/js/index.js
@@ -0,0 +1,9 @@
+menu.onclick = function () {
+  let elem = document.getElementById('myTopnav');
+
+  if (elem.className === 'topnav') {
+    elem.classList.add('responsive');
+  } else {
+    elem.className = 'topnav';
+  }
+}
